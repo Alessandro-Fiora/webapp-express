48c09dbb9dd994b902a1778ee623805a7620535c
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-01-10 16:38:12 +0100

    feat: add errorshandler and notfound middlewares

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 const express = require("express");
 const app = express();
 
+// ^ REGISTERING MIDDLEWARES
+app.use(express.json());
+app.use(express.static("public"));
+
 // ^ REGISTERING ROUTES
 const moviesRouter = require("./routers/moviesRouter");
 app.use("/movies", moviesRouter);
+
+// ^ ERROR HANDLERS
+const errorsHandler = require("./middlewares/errorsHandler");
+const notFound = require("./middlewares/notFound");
+app.use(errorsHandler);
+app.use(notFound);
 
 // ^ SERVER LISTENING
 app.listen(process.env.APP_PORT, () => {
